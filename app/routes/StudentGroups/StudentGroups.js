9e42956ac89041b8f7eb4471b52c7770f1d9fcc0
Billy <646017375@qq.com>
2018-04-17 21:21:09 +0800

    student group card ui

--- app/routes/StudentGroups/StudentGroups.js
+++ app/routes/StudentGroups/StudentGroups.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
-import { Row, Col } from 'antd'
+import { Row, Col, Button } from 'antd'
 
 import './StudentGroups.scss'
 
 export default class StudentGroups extends Component {
+  constructor (props) {
+    super(props)
+  }
   render () {
     return (
       <div className="studentgroups">
@@ -16,38 +19,34 @@ export default class StudentGroups extends Component {
 
           <Col span={24} xl={12}>
             <div className="group-card shadow-box">
-              <Row>
+              <Row type="flex">
                 <Col xs={24} sm={6}>
-                  <div className="group-card-left bg-gradient-5"></div>
+                  <Row
+                    className="group-card-left bg-gradient-5"
+                    type="flex"
+                    justify="center"
+                    align="middle">
+                    <Col>“习近平治国理政思想”读书社“</Col>
+                  </Row>
                 </Col>
-                <Col span={18}>
-                  <div></div>
-                </Col>
-              </Row>
-            </div>
-          </Col>
-
-          <Col span={24} xl={12}>
-            <div className="group-card shadow-box">
-              <Row>
-                <Col xs={24} sm={6}>
-                  <div className="group-card-left bg-gradient-3"></div>
-                </Col>
-                <Col span={18}>
-                  <div></div>
-                </Col>
-              </Row>
-            </div>
-          </Col>
-
-          <Col span={24} xl={12}>
-            <div className="group-card shadow-box">
-              <Row>
-                <Col xs={24} sm={6}>
-                  <div className="group-card-left bg-gradient-5"></div>
-                </Col>
-                <Col span={18}>
-                  <div></div>
+                <Col className="padding-20 group-card-right" xs={24} sm={18}>
+                  <Row>
+                    <Col>成立时间：2017年5月11日</Col>
+                  </Row>
+                  <Row>
+                    <Col>成员数：112人</Col>
+                  </Row>
+                  <Row>
+                    <Col>简介：集美大学学生法学社是经校团委审批的、受集美大学学生社团联合会指导的，系法律爱好者自愿结合的一个以法学学术性为主，集学术、实践及法律援助于一身的学生社团。社团以“弘扬法律精神，增强法治理念，探讨法律问题，提高法学素养”为宗旨，长期致力于法律服务工作，提供法律援助，进行法制宣传，努力成为一个有一定素养和专业的社团。社团组织开展了学术沙龙、模拟法庭表演、模拟法庭比赛、法律咨询、法律知识竞赛、案例讨论、校内外普法等一系列活动。</Col>
+                  </Row>
+                  <Row type="flex" justify="end" gutter={16}>
+                    <Col>
+                      <Button type="default">申请加入</Button>
+                    </Col>
+                    <Col>
+                      <Button type="default">社团管理</Button>
+                    </Col>
+                  </Row>
                 </Col>
               </Row>
             </div>
