91810c2b5f3d20deec289987422d5d93e252fd89
Billy <646017375@qq.com>
2018-05-14 02:28:51 +0800

    [backend]able to create approval

--- server/controllers/approvalController.js
+++ server/controllers/approvalController.js
@@ -3,5 +3,13 @@ const approvalService = require('../services/approvalService')
 const _ = require('lodash')
 
 module.exports = {
-
+  createApproval: (req, res) => {
+    let userId = req.userInfo._id.toString()
+    let approvalSchema = req.body.approvalSchema
+    approvalService.createApproval(userId, approvalSchema).then((result) => {
+      res.status(200).send(result)
+    }).catch((e) => {
+      res.status(400).send(e)
+    })
+  }
 }
--- server/services/approvalService.js
+++ server/services/approvalService.js
@@ -4,6 +4,28 @@ const mongoose = require('mongoose')
 const _ = require('lodash')
 
 const ApprovalService = {
+  createApproval: (userId, approvalSchema) => {
+    return new Promise((resolve, reject) => {
+      let newApproval = {
+        sponsorId: userId,
+        schemaId: approvalSchema._id,
+        status: 'notSubmit'
+      }
+      newApproval.approvalProcess = approvalSchema.approvalStack.map((step, index) => {
+        reutrn ({
+          approver: '',
+          status: 'waiting',
+          role: step.role
+        })
+      })
+      newApproval = new approvalModel(newApproval)
+      newApproval.save().then((result) => {
+        resolve(result)
+      }).catch((e) => {
+        reject(e)
+      })
+    })
+  }
 }
 
 module.exports = ApprovalService
