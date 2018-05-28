20a7bb46f1765c1a3b219cea46960270a1aacd7c
Billy <646017375@qq.com>
2018-05-28 09:42:29 +0800

    userinfo interface

--- app/routes/UserInfo/UserInfo.js
+++ app/routes/UserInfo/UserInfo.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Row, Col, Button, List, Avatar, Modal, Divider, Input, message, Select, Popconfirm } from 'antd'
+import { Row, Col, Button, List, Avatar, Modal, Divider, Input, message, Select, Popconfirm, Card } from 'antd'
 import classnames from 'classnames'
 import authService from '../../service/authService'
 
@@ -10,7 +10,12 @@ export default class UserInfo extends Component {
   constructor (props) {
     super(props)
     this.state = {
+      editModal: {
+        visible: false,
+        fields: {
 
+        }
+      }
     }
   }
   handleChange (path, result) {
@@ -21,8 +26,38 @@ export default class UserInfo extends Component {
     _.set(newState, path, result)
     this.setState(newState)
   }
+  toggleModal (modal) {
+    this.setState({
+      [modal]: {
+        ...this.state[modal],
+        visible: !this.state[modal].visible
+      }
+    })
+  }
   render () {
-
+    let user = this.props.userInfo
+    let data = [{
+      title: '学号/工号',
+      value: user.user
+    }, {
+      title: '性别',
+      value: user.gender
+    }, {
+      title: '出生日期',
+      value: user.birthDate
+    }, {
+      title: '身份证号',
+      value: user.CIdNumber
+    }, {
+      title: '民族',
+      value: user.ethnic
+    }, {
+      title: '家庭住址',
+      value: user.address
+    }, {
+      title: '手机号',
+      value: user.phoneNumber
+    }]
     return (
       <div className="userinfo">
         <Row className="page-title">
@@ -36,11 +71,38 @@ export default class UserInfo extends Component {
           <Col>
             <div className="shadow-box bg-white padding-20 margin-bottom-25">
               <h2>
-                用户名
+                基本信息
+                <Button
+                  className='float-right vertical-middle'
+                  shape="circle"
+                  icon='edit'
+                  onClick={this.toggleModal.bind(this, 'editModal')}
+                  size='large'/>
               </h2>
+              <Row>
+                <Col className='padding-20'>
+                  <List
+                    grid={{ gutter: 16, xs: 1, sm: 2, md: 4, lg: 4, xl: 6, xxl: 3 }}
+                    dataSource={data}
+                    renderItem={item => (
+                      <List.Item>
+                        <Card style={{textAlign:'center'}} type='inner' hoverable title={item.title}>{item.value}</Card>
+                      </List.Item>
+                    )}
+                  />
+                </Col>
+              </Row>
             </div>
           </Col>
         </Row>
+        <Modal
+          title="编辑用户信息"
+          visible={this.state.editModal.visible}
+          onOk={this.submitEdit}
+          onCancel={this.toggleModal.bind(this, 'editModal')}
+        >
+
+        </Modal>
       </div>
     )
   }
